5415e8720b8ef68595a6b85f41aaf9ffe1e0915a
ilyanic-hub <ilyanic20@gmail.com>
2026-07-21 01:33:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import asyncio
 import shutil
 import secrets
 import logging
+from sqlalchemy.orm import Session
 from dropbox.exceptions import ApiError  # 🌟 Добавь эту строчку!
 
 from concurrent.futures import ThreadPoolExecutor
